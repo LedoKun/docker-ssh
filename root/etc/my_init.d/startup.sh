09de23f348d2d27d7a1c04fd9b58d281401a7ea4
LedoKun <romamp100@gmail.com>
2018-08-17 21:49:15 +0700

    Generate password and ssh key locally (not at Dockerhub server)

--- root/etc/my_init.d/startup.sh
+++ root/etc/my_init.d/startup.sh
@@ -4,11 +4,20 @@ HOME=~project
 
 # Copy default sshd config
 if [ ! -h /etc/ssh/sshd_config  ]; then
-    cp /etc/ssh/default_password ${HOME}/default_password
+    # Regenerate ssh keys
+    /etc/my_init.d/00_regen_ssh_host_keys.sh > /dev/nul 2>&1
+
+    # Set the user passwd
+    USER_PASSWORD=`pwgen -c -n -1 10`
+    echo ${USER_PASSWORD} > ${HOME}/default_password
+    echo -e "${USER_PASSWORD}\n${USER_PASSWORD}" | passwd project > /dev/nul 2>&1
+    echo "Password for project user: ${USER_PASSWORD}"
+    echo "Please change your password!"
+
+    # Create symbolic link for sshd_config
+    rm -rf /etc/ssh/sshd_config
     cp /etc/ssh/sshd_config.original ${HOME}/sshd_config
     ln -s ${HOME}/sshd_config /etc/ssh/sshd_config
-
-    echo "Please change your password!"
 fi
 
 # Change UID & GID of the default user
--- root/install.sh
+++ root/install.sh
@@ -4,19 +4,17 @@
 set -e
 
 # PACKAGES
-BUILD_PACKAGES="pwgen"
-PACKAGES="sudo"
+PACKAGES="sudo \
+          pwgen"
 
 # Install BUILD_PACKAGES
 apt-get update
 apt-get dist-upgrade -y
 apt-get install -y \
-    ${BUILD_PACKAGES} \
     ${PACKAGES}
 
 # Enable sshd
 rm -f /etc/service/sshd/down
-/etc/my_init.d/00_regen_ssh_host_keys.sh > /dev/nul 2>&1
 
 # Backup config
 mv /etc/ssh/sshd_config /etc/ssh/sshd_config.original
@@ -24,15 +22,9 @@ mv /etc/ssh/sshd_config /etc/ssh/sshd_config.original
 # Config startup script
 chmod a+x /etc/my_init.d/*
 
-# Set the user passwd
-USER_PASSWORD=`pwgen -c -n -1 30`
-echo ${USER_PASSWORD} > /etc/ssh/default_password
-echo "project login password: $USER_PASSWORD"
-
-echo "project  ALL=(ALL:ALL) ALL" >> /etc/sudoers
-
 # Add User
-useradd -d ${HOME} -p ${USER_PASSWORD} -s /bin/bash project
+useradd -d ${HOME} -s /bin/bash project
+echo "project  ALL=(ALL:ALL) ALL" >> /etc/sudoers
 
 # Cleanup
 apt-get autoremove --purge -y ${BUILD_PACKAGES}
